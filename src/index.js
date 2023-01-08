8d47c114e71b85fbca7f496f0fba83173b43550f
erkaevrus <erk_08@mail.ru>
2023-01-08 15:30:57 +0500

    complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function toReadable (number) {
-  
+  if (number === 0) {return 'zero'}
+  const units = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+  const tens = ['', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+  let result = 'zero'
+  if (number < 20){
+    result = units[number]
+  } else if (number < 100) {
+    result = `${tens[Math.trunc(number / 10)]} ${units[number % 10]}`
+  } else {
+    if (number % 100 < 20) {
+      result = `${units[Math.trunc(number / 100 )]} hundred ${units[number % 100]}`
+    } else {
+      result = `${units[Math.trunc(number / 100 )]} hundred ${tens[Math.trunc(number % 100 / 10)]} ${units[number % 10]}`
+    }
+  }
+  return result.trimEnd()
 }
